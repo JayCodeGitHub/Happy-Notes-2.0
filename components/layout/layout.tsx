bcdb8b233d7c47198854f479cdd8f999e1aade86
JayCode Developer <JayCode.contakt@gmail.com>
2023-03-18 00:39:38 +0100

    Added global styles to all pages

--- components/layout/layout.tsx
+++ components/layout/layout.tsx
@@ -16,7 +16,11 @@ export default function Layout({ children }: LayoutProps) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Navbar />
-      <main>{children}</main>
+      <main className="flex items-center justify-center w-full min-h-screen bg-gradient-to-r from-gray-900 via-gray-700 to-green-900 auto-rows-min 2xl:grid-cols-4 xl:grid-cols-3 md:grid-cols-2 sm:grid-cols-1">
+        <div className="grid w-full min-h-screen bg-gradient-to-r dark:from-gray-900 from-white dark:via-gray-700 via-gray-400 dark:to-green-900 to-green-500 auto-rows-min 2xl:grid-cols-4 xl:grid-cols-3 md:grid-cols-2 sm:grid-cols-1">
+          {children}
+        </div>
+      </main>
     </>
   );
 }
